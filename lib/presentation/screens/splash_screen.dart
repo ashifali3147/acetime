528cef3772560cea42dc809844b4a68e7a1a4c5f
Ashif Ali <84494015+ashifali3147@users.noreply.github.com>
2025-08-23 20:42:10 +0530

    feat: splash_screen.dart added

---
+++ lib/presentation/screens/splash_screen.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../utils/storage_helper.dart';
+import '../navigation/route_names.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () => context.goNamed(getMainScreen()));
+  }
+
+  String getMainScreen() {
+    return StorageHelper().getLoginStatus()
+        ? RouteNames.home
+        : RouteNames.login;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Animate(
+          effects: [ShimmerEffect(duration: 3.seconds)],
+          onPlay: (controller) => controller.repeat(),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              "AceTime",
+              style: TextStyle(
+                color: Colors.deepPurpleAccent,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
